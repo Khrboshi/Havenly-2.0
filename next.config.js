5f78d9dfa0a8c8b73b7448ed6300841a3ca3ca3e
Larry <L.abbo@yahoo.com>
2025-11-14 17:36:57 +0200

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,8 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  images: {
-    domains: [],
-  },
+  experimental: {
+    serverActions: {
+      allowedOrigins: ["*"]
+    }
+  }
 };
 
 module.exports = nextConfig;
