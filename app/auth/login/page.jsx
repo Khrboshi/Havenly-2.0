d7a95df91c1afc22daca54034f8755376d8c4d15
Larry <L.abbo@yahoo.com>
2025-11-14 11:47:46 +0200

    Update page.jsx

--- app/auth/login/page.jsx
+++ app/auth/login/page.jsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { useState } from "react";
-import { signIn } from "@/lib/auth";
+import { signIn } from "../../../lib/auth"h";
 import Link from "next/link";
 
 export default function LoginPage() {
