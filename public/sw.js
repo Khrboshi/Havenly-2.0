e992f7974ac19be2507c63b13eb39a5f3e4d8662
Larry <L.abbo@yahoo.com>
2025-11-17 21:17:47 +0200

    Update sw.js

--- public/sw.js
+++ public/sw.js
@@ -1,26 +1,10 @@
-// --- SERVICE WORKER RESET MODE ---
-// This file unregisters old SWs and clears caches on all clients.
-
-self.addEventListener("install", (event) => {
+/* TEMPORARILY DISABLED SERVICE WORKER */
+self.addEventListener("install", () => {
   self.skipWaiting();
 });
 
-self.addEventListener("activate", async (event) => {
-  // Unregister all old service workers
-  try {
-    await self.registration.unregister();
-  } catch (e) {
-    console.error("SW unregister failed:", e);
-  }
-
-  // Delete all caches
-  try {
-    const keys = await caches.keys();
-    await Promise.all(keys.map((key) => caches.delete(key)));
-  } catch (e) {
-    console.error("Cache cleanup failed:", e);
-  }
-
-  // Take control immediately
-  self.clients.claim();
+self.addEventListener("activate", () => {
+  caches.keys().then(keys => {
+    keys.forEach(key => caches.delete(key));
+  });
 });
