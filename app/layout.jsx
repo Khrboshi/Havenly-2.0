090a23072a6a409618c3fad358e70bb69baf3858
Larry <L.abbo@yahoo.com>
2025-11-17 21:18:17 +0200

    Update layout.jsx

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,27 +1,21 @@
 "use client";
 
+import { useEffect } from "react";
 import "./globals.css";
-import { cn } from "@/lib/utils";
-import { Toaster } from "sonner";
-import BottomNav from "@/components/BottomNav";
 
 export default function RootLayout({ children }) {
+  useEffect(() => {
+    // TEMPORARILY DISABLE SW
+    if ("serviceWorker" in navigator) {
+      navigator.serviceWorker.getRegistrations().then(regs => {
+        regs.forEach(reg => reg.unregister());
+      });
+    }
+  }, []);
+
   return (
     <html lang="en">
-      <head>
-        <link rel="manifest" href="/manifest.json" />
-
-        {/* PWA metadata */}
-        <meta name="theme-color" content="#0D7A7E" />
-        <link rel="apple-touch-icon" href="/icons/apple-icon-180.png" />
-        <meta name="apple-mobile-web-app-capable" content="yes" />
-      </head>
-
-      <body className={cn("min-h-screen bg-background antialiased pb-16")}>
-        {children}
-        <BottomNav />
-        <Toaster />
-      </body>
+      <body>{children}</body>
     </html>
   );
 }
