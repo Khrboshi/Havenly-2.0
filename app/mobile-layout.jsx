ba3d4907d308853419be4ba457493af6f4a9464e
Larry <L.abbo@yahoo.com>
2025-11-17 21:23:55 +0200

    Update mobile-layout.jsx

--- app/mobile-layout.jsx
+++ app/mobile-layout.jsx
@@ -1,13 +1,27 @@
 "use client";
 
-import ForceReload from "@/app/force-reload";
+import "../globals.css";
+import BottomNav from "@/components/BottomNav";
+import { Toaster } from "sonner";
+import { useEffect } from "react";
 
 export default function MobileLayout({ children }) {
+  useEffect(() => {
+    // Ensure we removed any old service workers
+    if ("serviceWorker" in navigator) {
+      navigator.serviceWorker.getRegistrations().then(regs => {
+        regs.forEach(reg => reg.unregister());
+      });
+    }
+  }, []);
+
   return (
-    <div className="mobile-layout">
-      {/* 🚀 Ensure mobile PWA also refreshes */}
-      <ForceReload />
-      {children}
-    </div>
+    <html lang="en">
+      <body className="min-h-screen bg-background antialiased pb-16">
+        {children}
+        <BottomNav /> {/* <-- Add it here */}
+        <Toaster />
+      </body>
+    </html>
   );
 }
