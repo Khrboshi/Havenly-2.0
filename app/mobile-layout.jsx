b3dc5bb92308dde088e1d3984e2c3e32e46789e0
Larry <L.abbo@yahoo.com>
2025-11-17 21:29:41 +0200

    Update mobile-layout.jsx

--- app/mobile-layout.jsx
+++ app/mobile-layout.jsx
@@ -7,19 +7,19 @@ import { useEffect } from "react";
 
 export default function MobileLayout({ children }) {
   useEffect(() => {
-    // Ensure we removed any old service workers
+    // Remove old service workers on mobile
     if ("serviceWorker" in navigator) {
-      navigator.serviceWorker.getRegistrations().then(regs => {
-        regs.forEach(reg => reg.unregister());
+      navigator.serviceWorker.getRegistrations().then((regs) => {
+        regs.forEach((reg) => reg.unregister());
       });
     }
   }, []);
 
   return (
     <html lang="en">
-      <body className="min-h-screen bg-background antialiased pb-16">
-        {children}
-        <BottomNav /> {/* <-- Add it here */}
+      <body className="min-h-screen bg-background antialiased pb-20">
+        <main>{children}</main>
+        <BottomNav /> {/* Bottom bar always visible on mobile */}
         <Toaster />
       </body>
     </html>
