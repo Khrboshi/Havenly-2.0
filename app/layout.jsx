0991c1a0b83b0eb1e6f2104e5e84bc7a832ac45b
Larry <L.abbo@yahoo.com>
2025-11-17 14:30:02 +0200

    Update layout.jsx

--- app/layout.jsx
+++ app/layout.jsx
@@ -3,20 +3,33 @@
 import "./globals.css";
 import { cn } from "@/lib/utils";
 import { Toaster } from "sonner";
-import { useEffect } from "react";
 
 export default function RootLayout({ children }) {
-  useEffect(() => {
-    if ("serviceWorker" in navigator) {
-      navigator.serviceWorker.register("/sw.js");
-    }
-  }, []);
-
   return (
     <html lang="en">
+      <head>
+        {/* PWA meta */}
+        <link rel="manifest" href="/manifest.json" />
+        <link rel="apple-touch-icon" href="/icons/apple-icon-180.png" />
+        <meta name="theme-color" content="#000000" />
+        <meta name="mobile-web-app-capable" content="yes" />
+        <meta name="apple-mobile-web-app-capable" content="yes" />
+      </head>
+
       <body className={cn("min-h-screen bg-background antialiased")}>
         {children}
         <Toaster />
+
+        {/* Register service worker */}
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              if ("serviceWorker" in navigator) {
+                navigator.serviceWorker.register("/sw.js");
+              }
+            `,
+          }}
+        />
       </body>
     </html>
   );
