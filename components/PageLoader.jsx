87beeeeeeebfe6e48a0309f4c3738ca6e6ed1a56
Larry <L.abbo@yahoo.com>
2025-11-18 13:29:09 +0200

    Create PageLoader.jsx

---
+++ components/PageLoader.jsx
@@ -0,0 +1,7 @@
+export default function PageLoader() {
+  return (
+    <div className="w-full flex justify-center items-center py-20">
+      <p className="text-gray-500">Loading...</p>
+    </div>
+  );
+}
