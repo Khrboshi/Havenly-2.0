5ee9f16addc035b0cca329dc665a23df8d20b8cc
Larry <L.abbo@yahoo.com>
2025-11-18 14:15:04 +0200

    Update types.js

--- modules/insights/types.js
+++ modules/insights/types.js
@@ -1,1 +1,6 @@
-
+export const InsightsType = {
+  moodHistory: "Array<{ score: number, created_at: string }>",
+  journalCount: "number",
+  reflectionCount: "number",
+  averageMood: "number | null",
+};
