86378c0500eb5a80359d0087160cde991a9b5da1
Larry <L.abbo@yahoo.com>
2025-11-18 14:25:24 +0200

    Create types.js

---
+++ modules/ai/types.js
@@ -0,0 +1,9 @@
+export const AISummaryType = {
+  summary: "string",
+  sentiment: "string (positive | neutral | negative)",
+  keywords: "Array<string>",
+};
+
+export const AIMoodForecastType = {
+  forecast: "string",
+};
