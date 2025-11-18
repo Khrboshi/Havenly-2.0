3f1d353bcc14f5e0ce90854f271cbe9ecd99ec88
Larry <L.abbo@yahoo.com>
2025-11-18 13:13:53 +0200

    Create error.jsx

---
+++ app/(protected)/journal/error.jsx
@@ -0,0 +1,11 @@
+"use client";
+
+export default function Error() {
+  return (
+    <div className="w-full flex justify-center items-center py-10">
+      <p className="text-red-500 font-medium">
+        Something went wrong. Please try again.
+      </p>
+    </div>
+  );
+}
