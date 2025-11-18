4d7835cb3e41938fbe0b93addd3904504e874b66
Larry <L.abbo@yahoo.com>
2025-11-18 13:28:32 +0200

    Create Header.jsx

---
+++ components/Header.jsx
@@ -0,0 +1,9 @@
+"use client";
+
+export default function Header() {
+  return (
+    <header className="w-full py-4 px-4 bg-white shadow-sm">
+      <h1 className="text-xl font-semibold text-[#0D7A7E]">Havenly</h1>
+    </header>
+  );
+}
