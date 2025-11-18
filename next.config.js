9b6eafe78639d47cc7011d9196653b6c7838b051
Larry <L.abbo@yahoo.com>
2025-11-18 11:13:28 +0200

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,16 +1,11 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  experimental: {
-    serverActions: true
-  },
-  images: {
-    domains: []
-  }
-};
-
 const withPWA = require("next-pwa")({
   dest: "public",
   disable: process.env.NODE_ENV === "development"
 });
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: { domains: [] }
+};
 
 module.exports = withPWA(nextConfig);
