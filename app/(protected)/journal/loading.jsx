dc3b3203621d34b0db0b83214de5152652e03f85
Larry <L.abbo@yahoo.com>
2025-11-18 13:11:35 +0200

    Create loading.jsx

---
+++ app/(protected)/journal/loading.jsx
@@ -0,0 +1,7 @@
+export default function Loading() {
+  return (
+    <div className="w-full flex justify-center items-center py-10">
+      <p className="text-gray-500">Loading...</p>
+    </div>
+  );
+}
