798002a029fedd03fb6780e07a377542c338f393
Larry <L.abbo@yahoo.com>
2025-11-18 13:33:15 +0200

    Create DailyNudge.jsx

---
+++ components/DailyNudge.jsx
@@ -0,0 +1,9 @@
+"use client";
+
+export default function DailyNudge({ message }) {
+  return (
+    <div className="bg-white border border-gray-200 rounded-lg p-4 shadow-sm my-4">
+      <p className="text-gray-700 text-sm leading-relaxed">{message}</p>
+    </div>
+  );
+}
