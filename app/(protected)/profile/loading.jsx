a9cfb69bf7aeac3354a340bc2bebd5f733ddde94
Larry <L.abbo@yahoo.com>
2025-11-20 17:59:20 +0200

    Delete app/(protected)/profile/loading.jsx

--- app/(protected)/profile/loading.jsx
+++
@@ -1,7 +0,0 @@
-export default function Loading() {
-  return (
-    <div className="w-full flex justify-center items-center py-10">
-      <p className="text-gray-500">Loading...</p>
-    </div>
-  );
-}
