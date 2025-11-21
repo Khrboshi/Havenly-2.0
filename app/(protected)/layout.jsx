fdf7ca0baa5851cfa148f8c432532d79fc074bd7
Larry <L.abbo@yahoo.com>
2025-11-21 17:40:45 +0200

    Update layout.jsx

--- app/(protected)/layout.jsx
+++ app/(protected)/layout.jsx
@@ -1,14 +1,14 @@
-import "../styles.css"; // Correct relative path (protected → app)
-
-import AddToHomeScreen from "@/components/AddToHomeScreen";
 import Navbar from "@/components/Navbar";
+import "../styles.css";
 
 export default function ProtectedLayout({ children }) {
   return (
-    <>
+    <div className="min-h-screen bg-[#F7FBFA]">
       <Navbar />
-      <AddToHomeScreen />
-      <main>{children}</main>
-    </>
+
+      <main className="max-w-4xl mx-auto px-6 py-10">
+        {children}
+      </main>
+    </div>
   );
 }
