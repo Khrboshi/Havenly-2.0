61d4d623ca1f65b7d8083fb99e5bd1f3634037b3
Larry <L.abbo@yahoo.com>
2025-11-21 16:50:29 +0200

    Update layout.jsx

--- app/(protected)/layout.jsx
+++ app/(protected)/layout.jsx
@@ -1,11 +1,11 @@
-import "@/app/globals.css";
-import Navbar from "@/components/Navbar";
+import "../../globals.css";
+import Header from "@/components/Header";
 import AddToHomeScreen from "@/components/AddToHomeScreen";
 
 export default function ProtectedLayout({ children }) {
   return (
     <div>
-      <Navbar />
+      <Header />
       <AddToHomeScreen />
       <main className="pt-20 max-w-3xl mx-auto px-4">{children}</main>
     </div>
