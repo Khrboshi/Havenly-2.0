3d724378ce6e106afc618116bd75cd13ece1686d
Larry <L.abbo@yahoo.com>
2025-11-21 14:50:48 +0200

    Update sonner.jsx

--- components/ui/sonner.jsx
+++ components/ui/sonner.jsx
@@ -3,5 +3,12 @@
 import { Toaster } from "sonner";
 
 export function SonnerToaster() {
-  return <Toaster richColors closeButton />;
+  return (
+    <Toaster
+      richColors
+      closeButton
+      position="bottom-center"
+      expand={false}
+    />
+  );
 }
