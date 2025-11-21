d0fe8e11c987ffd25b627f3c658f7b6edd9943bd
Larry <L.abbo@yahoo.com>
2025-11-21 14:51:57 +0200

    Update ErrorFallback.jsx

--- components/ErrorFallback.jsx
+++ components/ErrorFallback.jsx
@@ -1,6 +1,11 @@
-export default function ErrorFallback({ message = "Something went wrong." }) {
+export default function ErrorFallback({
+  message = "Something went wrong.",
+  className = "",
+}) {
   return (
-    <div className="bg-red-50 border border-red-200 text-red-700 p-4 rounded-lg text-sm">
+    <div
+      className={`bg-red-50 border border-red-200 text-red-700 p-4 rounded-lg text-sm ${className}`}
+    >
       {message}
     </div>
   );
