37cd68968c32147b32e4b6c61d546d82f956e463
Larry <L.abbo@yahoo.com>
2025-11-21 14:53:33 +0200

    Update PageLoader.jsx

--- components/PageLoader.jsx
+++ components/PageLoader.jsx
@@ -1,7 +1,7 @@
 export default function PageLoader() {
   return (
     <div className="w-full flex justify-center items-center py-20 text-gray-500">
-      <div className="animate-pulse">Loading...</div>
+      <div className="animate-pulse text-sm">Loading…</div>
     </div>
   );
 }
