d80e921c95f4036142163f845f5d18c2917fead2
Larry <L.abbo@yahoo.com>
2025-11-22 12:05:13 +0200

    Update layout.jsx

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,5 +1,3 @@
---- FILE: app/layout.jsx ---
-
 import "./styles.css";
 import { Inter } from "next/font/google";
 import Header from "@/components/Header";
@@ -17,8 +15,6 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en" suppressHydrationWarning>
       <body className={`${inter.className} bg-[#F7FBFA] text-gray-900`}>
-
-        {/* Global toast notifications */}
         <SonnerToaster />
 
         <div className="min-h-screen flex flex-col">
